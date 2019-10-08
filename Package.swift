1b3ecc8b76c4e26a250c4361e59fc7a9abac7a24
Sam Spencer <samuels5@mail.usf.edu>
2019-10-08 19:36:05 -0400

    Update Package Structure
    Should finally work...

--- Package.swift
+++ Package.swift
@@ -3,16 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "PullToDismiss",
-    path: "Sources/",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15)
     ],
     products: [
-        .library(name: "PullToDismiss", targets: ["PullToDismiss", "PullToDismissTests"])
+        .library(name: "PullToDismiss", targets: ["PullToDismiss"])
     ],
     targets: [
-        .target(name: "PullToDismiss"),
-        .testTarget(name: "PullToDismissTests")
+        .target(name: "PullToDismiss")
     ]
 )
